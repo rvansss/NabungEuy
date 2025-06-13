338db10c481c2441b81005ae9ade48ce70e41534
Evansius Rafael Simbolon <rvansius13@gmail.com>
2025-06-13 22:13:17 +0700

    Membuat tampilan nama hewan

--- app/src/main/java/com/evan0107/nabungeuy/saving/SavingScreen.kt
+++ app/src/main/java/com/evan0107/nabungeuy/saving/SavingScreen.kt
@@ -1,21 +1,28 @@
 package com.evan0107.nabungeuy.saving
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
@@ -46,7 +53,8 @@ fun SavingScreen(
 @Composable
 fun ListItem(hobiItem: HobiItem) {
     Box(
-        modifier = Modifier.padding(4.dp).border(1.dp, Color.Gray)
+        modifier = Modifier.padding(4.dp).border(1.dp, Color.Gray),
+        contentAlignment = Alignment.BottomCenter
     ) {
         AsyncImage(
             model = ImageRequest.Builder(LocalContext.current)
@@ -57,5 +65,29 @@ fun ListItem(hobiItem: HobiItem) {
             contentScale = ContentScale.Crop,
             modifier = Modifier.fillMaxWidth().padding(4.dp)
         )
+        Column (
+            modifier = Modifier.fillMaxWidth().padding(4.dp)
+                .background(Color(red = 0f, green = 0f, blue = 0f, alpha = 0.5f))
+                .padding(4.dp)
+        ){
+            Text(
+                text = hobiItem.name,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+            Text(
+                text = hobiItem.brand,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+//            Text(
+//                text = hobiItem.price,
+//                fontWeight = FontStyle.Italic,
+//                fontSize = 14.sp,
+//                color = Color.White
+//
+//            )
+
+        }
     }
 }
