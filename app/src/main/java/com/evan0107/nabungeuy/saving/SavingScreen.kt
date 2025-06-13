aa6ee5382f86dd8ba714aafaba45b36099edb857
Evansius Rafael Simbolon <rvansius13@gmail.com>
2025-06-13 22:50:10 +0700

    Menangani gambar gagal di-load

--- app/src/main/java/com/evan0107/nabungeuy/saving/SavingScreen.kt
+++ app/src/main/java/com/evan0107/nabungeuy/saving/SavingScreen.kt
@@ -65,6 +65,7 @@ fun ListItem(hobiItem: HobiItem) {
             contentDescription = stringResource(R.string.gambar, hobiItem.name),
             contentScale = ContentScale.Crop,
             placeholder = painterResource(id = R.drawable.loading_img),
+            error = painterResource(id = R.drawable.broken_img),
             modifier = Modifier.fillMaxWidth().padding(4.dp)
         )
         Column (
